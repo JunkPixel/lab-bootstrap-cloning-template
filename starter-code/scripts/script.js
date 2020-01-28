8b9e710dfb924a8b0302d8404a33d77e0015d24b
unknown <ricardotomaz027@gmail.com>
2020-01-28 14:08:51 +0000

    not done

---
+++ starter-code/scripts/script.js
@@ -0,0 +1,18 @@
+Your go-to digital agency Significa is a digital design-led agency mainly focused on Product
+    Development. Oopsie for users — Your favourite food, one click away! Business Analysis, Product
+    Management, Consultancy & 5 others Opennode — Bitcoin, for everyday. UI/UX, Front End, Front End
+    AKASHA — Open systems to expand our collective mind. UI/UX, Front End, Illustration We build
+    digital products Having a design-led approach to what we do defines who we are. We take on
+    products from inception to launch, from business model to people's pocket, from wireframe to
+    continuous deployment. Strategy UX research Business strategy Brand strategy Product strategy
+    Product management Product design Product design User experience User interface Brand
+    development Illustration Motion & Video Development Development Front-end engineering Web-app
+    development Native app development Development operations View services View services What's new
+    on Significa Labs Automating releases for UI Kits — How to automate release logs and builds for
+    your style guides Tutorial on Medium An iceberg sank the Titanic — But what’s that got to do
+    with text hierarchy? Article on Medium On creating synergies — Sharing without compromising our
+    identity Article on Medium Enter the Lab Drop by, we're hiring. We are an open-minded,
+    open-hearted team that loves the thrill of creating exciting new projects. Drop us a line if
+    you’re passionate and talented to do everything you set your mind to. View careers Legal © 2019
+    Say hello@significa.co (+351) 226 001 751 Links Home Showcase Services About Careers Labs
+    Contact Network Coletiv Adamant Follow us 
